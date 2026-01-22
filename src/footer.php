c3c9385b5ad4eddbc352388692a0ecc33ee745e9
alici69b <aliciaalonsoaguaded@gmail.com>
2026-01-22 20:45:06 +0100

    Cambio de Header y Footer y algo del inicio.php

--- src/footer.php
+++ src/footer.php
@@ -19,7 +19,7 @@
 </head>
 <body class="bg-gray-100 font-poppins m-auto">
 
-    <footer class="bg-gray-900 text-white py-12 mt-12 ">
+    <footer class="bg-gray-900 text-white py-12 mt-12  bottom-0 left-0 right-0">
         <div class="max-w-7xl mx-auto px-6">
             <div class="flex justify-around md:justify-around-12 gap-8">
                 
