ba0b4300861dfae745c7e4e021e5c3985ad66364
alici69b <aliciaalonsoaguaded@gmail.com>
2026-01-26 10:57:49 +0100

    Creacion de la pagina contacto y modificacion de la pagina de inicio.php

--- src/Header.php
+++ src/Header.php
@@ -31,7 +31,7 @@
 
             <!-- Botón hamburguesa -->
             <label for="menu-toggle" class="cursor-pointer z-20 block">
-                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0,0,256,256">
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="35" height="35" viewBox="0,0,256,256">
                 <g fill="#ff3b30" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(10.66667,10.66667)"><path d="M2,5v2h20v-2zM2,11v2h20v-2zM2,17v2h20v-2z"></path></g></g>
                 </svg>
             </label>
