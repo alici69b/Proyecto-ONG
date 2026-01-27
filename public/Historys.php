e84d3236735467beb143921e4b109a04bb099653
alici69b <aliciaalonsoaguaded@gmail.com>
2026-01-27 10:06:49 +0100

    Modificaciones estilos

--- public/Historys.php
+++ public/Historys.php
@@ -29,11 +29,7 @@
 
     <main class="max-w-7xl mx-auto px-4 py-12">
         <div>
-            <!-- Botón volver Inicio -->
-            <a href="Inicio.php" class="text-sm hover:text-gray-900 flex items-center gap-2 text-gray-500">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M5 12l4 4" /><path d="M5 12l4 -4" /></svg>
-                Volver al Inicio
-            </a>
+            
             
             <!-- Título -->
              <div class="text-center my-12">
