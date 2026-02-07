ad837ed230789edd3a51487f143b6e7c199724a8
alici69b <aliciaalonsoaguaded@gmail.com>
2026-02-08 00:20:15 +0100

    Organizacion

---
+++ app/config/db.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$user = "root";
+$pwd = "";
+$dbname = "reset_db";
+
+$conexion = mysqli_connect($servername, $user, $pwd, $dbname) or die("No se ha podido conectar");
+
+?>
