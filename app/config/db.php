66789cbb74a4f3fd927cf959276f572b3c8f4583
alici69b <aliciaalonsoaguaded@gmail.com>
2026-02-11 11:07:34 +0100

    Modificaciones de errores en el registro

--- app/config/db.php
+++ app/config/db.php
@@ -7,7 +7,7 @@ $base_datos = "Proyecto_ong";
 // Guardamos la conexion en una variable
 $conexion = mysqli_connect($servidor, $usuario, $password, $base_datos);
 
-// Miramos si hemos hecho bien la conexionn, si no, manda un mensaje
+// Miramos si hemos hecho bien la conexionn, si no, manda un mensaje con el problema detallado
 if (!$conexion) {
     die("Error al conectar: " . mysqli_connect_error());
 }
