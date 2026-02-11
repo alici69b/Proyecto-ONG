a19f61955a6f88f028fb65168e0fd46e89b12884
alici69b <aliciaalonsoaguaded@gmail.com>
2026-02-11 09:24:05 +0100

    Configuracion del header, enlaces y la conexion de la base de datos ya creada.

---
+++ app/config/db.php
@@ -0,0 +1,16 @@
+<?php
+$servidor = "localhost";
+$usuario  = "root";
+$password = "";
+$base_datos = "Proyecto_ong";
+
+// Guardamos la conexion en una variable
+$conexion = mysqli_connect($servidor, $usuario, $password, $base_datos);
+
+// Miramos si hemos hecho bien la conexionn, si no, manda un mensaje
+if (!$conexion) {
+    die("Error al conectar: " . mysqli_connect_error());
+}
+
+echo "Conectado correctamente a Proyecto_ong";
+?>
